5b37fb029ef868cb4a467b4d864a81b2e921ba11
dixlune <elyse.castilloux@usherbrooke.ca>
2025-04-20 14:39:22 -0400

    finaliser les requêtes

--- _targets.R
+++ _targets.R
@@ -8,7 +8,6 @@
   #Appeler les scripts
   source('Fonctions/fonction_nettoyerdata.R')
   source('Fonctions/creer_tables.R')
-  # source('Fonctions/requete.R')
   source('Fonctions/requete_temp.R')
   # source('Fonctions/figure_resume.R')
   tar_option_set(packages = c("dplyr", "readr", "DBI", "RSQLite", "janitor", "ggplot2"))  #mettre les libraries qu'on aura besoin dans le target dans le vecteur (c())
@@ -27,26 +26,28 @@ list(
     command = creerBD(final_data_clean, db_name = "reseau.db")
   ),
   
-  # requête température
+  #requête température
   tar_target(
     name = Requete_temperature,
     command = fonction_requete_temp(reseau.db$con)
 
-  ))  
-  
-  # requête courant
- # tar_target(
- #   name = Requete_courant,
-#    command = fonction_requete_cou(reseau.db$con)
-    
-#  ),
+  ),
+
+  #requête courant
+  tar_target(
+    name = Requete_courant,
+    command = fonction_requete_cou(reseau.db$con)
+
+  ),
   
   # requête profondeur
-#  tar_target(
- #   name = Requete_profondeur,
-  #  command = fonction_requete_pro(reseau.db$con)
-    
- # )
+  tar_target(
+    name = Requete_profondeur,
+    command = fonction_requete_pro(reseau.db$con)
+  
+  )
+  
+  #
   
   # Ensuite créer le fichier Rmarkdown
 #)
@@ -58,15 +59,15 @@ list(
 #faire un nettoyage des données :
 
 # 
-source('Fonctions/fonction_nettoyerdata.R')
-directory_path <- "DATA"
-final_data_clean<-process_data(directory_path)
+#source('Fonctions/fonction_nettoyerdata.R')
+#directory_path <- "DATA"
+#final_data_clean<-process_data(directory_path)
 # print(head(final_data_clean))
 # 
 # #Créer les bases de données et injecter les données dans les tables de notre fonction
 # 
-source('Fonctions/creer_tables.R')
-creerBD(final_data_clean, db_name = "reseau.db")
+#source('Fonctions/creer_tables.R')
+#creerBD(final_data_clean, db_name = "reseau.db")
 # #tester notre BD avec des requêtes 
 # source('Fonctions/requete.R')
 
