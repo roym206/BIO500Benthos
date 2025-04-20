bb77d84b8b436fc680da9150e835660db0c734bd
dixlune <elyse.castilloux@usherbrooke.ca>
2025-04-20 17:37:14 -0400

    rajout guillemets

--- _targets.R
+++ _targets.R
@@ -32,21 +32,21 @@ list(
   #requête température
   tar_target(
     name = Requete_temperature,
-    command = fonction_requete_temp(reseau.db)
+    command = fonction_requete_temp("reseau.db")
 
   ),
 
   #requête courant
   tar_target(
     name = Requete_courant,
-    command = fonction_requete_cou(reseau.db)
+    command = fonction_requete_cou("reseau.db")
 
   ),
   
   # requête profondeur
   tar_target(
     name = Requete_profondeur,
-    command = fonction_requete_pro(reseau.db)
+    command = fonction_requete_pro("reseau.db")
   
   ),
   
