00945aa0014cb5ce3fa721c2492c5fc6a1f65227
roym206 <momoroy01@gmail.com>
2025-04-22 12:44:10 -0400

    Ajustements finaux du script principal

--- execution.R
+++ execution.R
@@ -3,19 +3,6 @@ library(RSQLite)
 library(targets)
 tar_make()
 tar_visnetwork()
-tar_read(benthos)
-tar_read(reseau.db)
- 
-targets::tar_read(reseau.db) %>% collect()
-targets::tar_read(tbl_emplacement) %>% collect()
-
-DBI::dbIsValid(db_objects$con)
-
-dbGetQuery(db_objects$con, "SELECT * FROM emplacement")
-dbGetQuery(db_objects$con, "SELECT * FROM benthos")
-
-
-
 
 
 
